f1cc773fcb82a1aee2bd54e6637e7856ab620ebc
m0bar3z <amobarez00@gmail.com>
2024-02-25 19:07:27 +0330

    feat: eslint and prettier

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,37 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const path = require("path");
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import { resolve as _resolve } from "path";
 
-module.exports = {
-  entry: "./src/index.tsx",
-  mode: "development",
-  output: {
-    filename: "bundle.[fullhash].js",
-    path: path.resolve(__dirname, "dist")
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/index.html"
-    })
-  ],
-  resolve: {
-    modules: [__dirname, "src", "node_modules"],
-    extensions: ["*", ".js", ".jsx", ".tsx", ".ts"]
-  },
-  module: {
-    rules: [
-      {
-        test:  /\.(js|ts)x?$/,
-        exclude: /node_modules/,
-        use: ["babel-loader"]
-      },
-      {
-        test: /\.css$/,
-        exclude: /node_modules/,
-        use: ["style-loader", "css-loader"]
-      },
-      {
-        test: /\.(png|svg|jpg|gif)$/,
-        exclude: /node_modules/,
-        use: ["file-loader"]
-      }
-    ]
-  }
-}
+export const entry = "./src/index.tsx";
+export const mode = "development";
+export const output = {
+  filename: "bundle.[fullhash].js",
+  path: _resolve(__dirname, "dist")
+};
+export const plugins = [
+  new HtmlWebpackPlugin({
+    template: "./src/index.html"
+  })
+];
+export const resolve = {
+  modules: [__dirname, "src", "node_modules"],
+  extensions: ["*", ".js", ".jsx", ".tsx", ".ts"]
+};
+export const module = {
+  rules: [
+    {
+      test: /\.(js|ts)x?$/,
+      exclude: /node_modules/,
+      use: ["babel-loader"]
+    },
+    {
+      test: /\.css$/,
+      exclude: /node_modules/,
+      use: ["style-loader", "css-loader"]
+    },
+    {
+      test: /\.(png|svg|jpg|gif)$/,
+      exclude: /node_modules/,
+      use: ["file-loader"]
+    }
+  ]
+};
