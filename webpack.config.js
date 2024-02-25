e98f0bbf17fbd7a620306458eb6d707fadf598cb
m0bar3z <amobarez00@gmail.com>
2024-02-25 19:30:33 +0330

    change: export default for webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,41 @@
 import HtmlWebpackPlugin from "html-webpack-plugin";
 import { resolve as _resolve } from "path";
+import path from 'path';
+const __dirname = path.resolve();
 
-export const entry = "./src/index.tsx";
-export const mode = "development";
-export const output = {
-  filename: "bundle.[fullhash].js",
-  path: _resolve(__dirname, "dist")
-};
-export const plugins = [
-  new HtmlWebpackPlugin({
-    template: "./src/index.html"
-  })
-];
-export const resolve = {
-  modules: [__dirname, "src", "node_modules"],
-  extensions: ["*", ".js", ".jsx", ".tsx", ".ts"]
-};
-export const module = {
-  rules: [
-    {
-      test: /\.(js|ts)x?$/,
-      exclude: /node_modules/,
-      use: ["babel-loader"]
-    },
-    {
-      test: /\.css$/,
-      exclude: /node_modules/,
-      use: ["style-loader", "css-loader"]
-    },
-    {
-      test: /\.(png|svg|jpg|gif)$/,
-      exclude: /node_modules/,
-      use: ["file-loader"]
-    }
-  ]
+export default {
+  entry: "./src/index.tsx",
+  mode: "development",
+  output: {
+    filename: "bundle.[fullhash].js",
+    path: path.join(__dirname, "dist")
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html"
+    })
+  ],
+  resolve: {
+    modules: [__dirname, "src", "node_modules"],
+    extensions: ["*", ".js", ".jsx", ".tsx", ".ts"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|ts)x?$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"]
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        exclude: /node_modules/,
+        use: ["file-loader"]
+      }
+    ]
+  }
 };
